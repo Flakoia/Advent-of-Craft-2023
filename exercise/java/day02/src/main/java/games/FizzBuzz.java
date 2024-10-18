8cf652c176d37356ce9ff3170799e45222c2d98e
qchaille <qchaille@cross-systems.com>
2024-10-18 14:15:24 +0200

    remove complexity

--- exercise/java/day02/src/main/java/games/FizzBuzz.java
+++ exercise/java/day02/src/main/java/games/FizzBuzz.java
@@ -1,27 +1,40 @@
 package games;
 
 public class FizzBuzz {
+
+    public static final int BUZZ_MULTIPLIER = 5;
+    public static final int FIZZ_MULTIPLIER = 3;
+
     private FizzBuzz() {
     }
     
     public static String convert(Integer input) throws OutOfRangeException {
-        if (input > 0) {
-            if (input <= 100) {
-                if (input % 3 == 0 && input % 5 == 0) {
-                    return "FizzBuzz";
-                }
-                if (input % 3 == 0) {
-                    return "Fizz";
-                }
-                if (input % 5 == 0) {
-                    return "Buzz";
-                }
-                return input.toString();
-            } else {
-                throw new OutOfRangeException();
-            }
-        } else {
+        if (isOutOfRange(input)) {
             throw new OutOfRangeException();
         }
+
+        return convertIntegerToFizzBuzz(input);
+    }
+
+    private static String convertIntegerToFizzBuzz(Integer input) {
+        if (isMultipleOf(input, BUZZ_MULTIPLIER) && isMultipleOf(input, FIZZ_MULTIPLIER)) {
+            return "FizzBuzz";
+        }
+        if (isMultipleOf(input, FIZZ_MULTIPLIER)) {
+            return "Fizz";
+        }
+        if (isMultipleOf(input, BUZZ_MULTIPLIER)) {
+            return "Buzz";
+        }
+
+        return input.toString();
+    }
+
+    private static boolean isOutOfRange(Integer input) {
+        return input <= 0 || input >= 100;
+    }
+
+    private static boolean isMultipleOf(Integer input, Integer modulo) {
+        return input % modulo == 0;
     }
 }
