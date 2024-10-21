ed196d3658abd674953085d7991e5fddbbb16fe2
qchaille <qchaille@cross-systems.com>
2024-10-21 14:40:07 +0200

    day04

--- exercise/java/day04/src/test/java/blog/ArticleTests.java
+++ exercise/java/day04/src/test/java/blog/ArticleTests.java
@@ -1,71 +1,50 @@
 package blog;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ArticleTests {
-    @Test
-    void it_should_add_valid_comment() throws CommentAlreadyExistException {
-        var article = new Article(
+    public static final String COMMENT_TEXT = "Amazing article !!!";
+    public static final String COMMENT_AUTHOR = "Pablo Escobar";
+    private Article article;
+
+    @BeforeEach
+    public void createArticleWithComment() throws CommentAlreadyExistException {
+        article = new Article(
                 "Lorem Ipsum",
                 "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
         );
 
-        article.addComment("Amazing article !!!", "Pablo Escobar");
+        article.addComment(COMMENT_TEXT, COMMENT_AUTHOR);
     }
 
     @Test
-    void it_should_add_a_comment_with_the_given_text() throws CommentAlreadyExistException {
-        var article = new Article(
-                "Lorem Ipsum",
-                "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-        );
+    void it_should_add_a_comment_in_an_article() {
+               assertThat(article.getComments())
+                .hasSize(1)
+                .anyMatch(comment -> comment.text().equals(COMMENT_TEXT))
+                .anyMatch(comment -> comment.author().equals(COMMENT_AUTHOR));
+    }
 
-        var text = "Amazing article !!!";
-        article.addComment(text, "Pablo Escobar");
+    @Test
+    void it_should_add_different_comments_in_an_article() throws CommentAlreadyExistException {
+        article.addComment("It's magic !", "Harry Potter");
 
         assertThat(article.getComments())
-                .hasSize(1)
-                .anyMatch(comment -> comment.text().equals(text));
+                .hasSize(2)
+                .anyMatch(comment -> comment.text().equals(COMMENT_TEXT))
+                .anyMatch(comment -> comment.text().equals("It's magic !"))
+                .anyMatch(comment -> comment.author().equals(COMMENT_AUTHOR))
+                .anyMatch(comment -> comment.author().equals("Harry Potter"));
     }
 
     @Test
-    void it_should_add_a_comment_with_the_given_author() throws CommentAlreadyExistException {
-        var article = new Article(
-                "Lorem Ipsum",
-                "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-        );
-
-        var author = "Pablo Escobar";
-        article.addComment("Amazing article !!!", author);
-
-        assertThat(article.getComments())
-                .hasSize(1)
-                .anyMatch(comment -> comment.author().equals(author));
-    }
-
-    @Test
-    void it_should_add_a_comment_with_the_date_of_the_day() throws CommentAlreadyExistException {
-        var article = new Article(
-                "Lorem Ipsum",
-                "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-        );
-
-        article.addComment("Amazing article !!!", "Pablo Escobar");
-    }
-
-    @Test
-    void it_should_throw_an_exception_when_adding_existing_comment() throws CommentAlreadyExistException {
-        var article = new Article(
-                "Lorem Ipsum",
-                "consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore"
-        );
-        article.addComment("Amazing article !!!", "Pablo Escobar");
-
+    void it_should_throw_an_exception_when_adding_existing_comment() {
         assertThatThrownBy(() -> {
-            article.addComment("Amazing article !!!", "Pablo Escobar");
+            article.addComment(COMMENT_TEXT, COMMENT_AUTHOR);
         }).isInstanceOf(CommentAlreadyExistException.class);
     }
 }
